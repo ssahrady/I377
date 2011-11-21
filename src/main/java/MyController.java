3619b7179edb61c03eff83c6d631e053d6727685
Taivo Türnpu <taivotyrnpu@gmail.com>
2011-11-21 23:15:41 +0200

    project layout

---
+++ src/main/java/MyController.java
@@ -0,0 +1,16 @@
+package ee.taivo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/hello")
+    public ModelAndView helloWorld() {
+
+        String message = "Hello World, Spring 3.0!";
+        return new ModelAndView("hello", "message", message);
+    }
+}
