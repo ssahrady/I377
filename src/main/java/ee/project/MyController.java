ed000ad646023f1e6006778a455379715833a16b
Taivo Türnpu <taivotyrnpu@gmail.com>
2011-11-26 18:13:01 +0200

    test

--- src/main/java/ee/project/MyController.java
+++ src/main/java/ee/project/MyController.java
@@ -20,7 +20,7 @@ public class MyController {
     @RequestMapping("/hello")
     public ModelAndView helloWorld() {
 
-        String message = "HelloWorld, Spring 3.0!";
+        String message = "HelloWaaorld, Spring 3.0!";
 
         //int a = getMyDAOImpl().makeObjekti_liik();
 
